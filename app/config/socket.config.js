b033dfe81252db73d5b9d0a7f7de65caa8e2738a
lngka <foxlead@gmail.com>
2017-10-16 21:14:54 +0200

    server read database and send symbols to client

--- app/config/socket.config.js
+++ app/config/socket.config.js
@@ -1,9 +1,25 @@
 "use strict";
+const fs = require("fs");
+const readline = require("readline");
+const path = require("path");
 
 module.exports = function(io) {
     io.on("connect", function(socket) {
-        console.log("Received connection from id: " + socket.id);
-        socket.emit("message", {"message": "I heard you, hi", "clientID": socket.id});
-    });
 
+        // read symbols from a text file and send it to client
+        var file = path.join(process.cwd(), "app", "data", "symbols.txt");
+        var lineReader = readline.createInterface({
+            "input": fs.createReadStream(file)
+        });
+
+        var symbols = [];
+        lineReader.on("line", function(line) {
+            symbols.push(line);
+        });
+
+        lineReader.on("close", function() {
+            socket.emit("symbols", {"symbols": symbols});
+        });
+
+    });
 };
--- public/js/main.js
+++ public/js/main.js
@@ -3,18 +3,18 @@
 $(function() {
     const socket = SocketController.getSocket(window.location.origin);
 
-    socket.on("message", function(data) {
-        console.log("Got a message from server: ");
-        console.log(data);
-    });
+    socket.on("symbols", function(data) {
+        var symbols = data.symbols;
+        console.log(data.symbols);
+        StockDataController.getBySymbol(symbols[0], function(err, data){
+            if (err) {
+                return alert(err.message);
+            }
+            // drawing chart
+            var canvas = document.querySelector("#chart-area").id;
+            //eslint-disable-next-line no-undef
+            HighstockController.draw(canvas, data);
+        });
 
-    StockDataController.getBySymbol("MSFT", function(err, data){
-        if (err) {
-            return alert(err.message);
-        }
-        // drawing chart
-        var canvas = document.querySelector("#chart-area").id;
-        //eslint-disable-next-line no-undef
-        HighstockController.draw(canvas, data);
     });
 });
